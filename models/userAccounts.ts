cda521c4a4085958810c7d32f04b825af5f49017
dardeejay <dardeejay.calura28@gmail.com>
2023-04-14 14:47:10 +0800

    update

--- models/userAccounts.ts
+++ models/userAccounts.ts
@@ -1,8 +1,10 @@
-import mongoose, { Document, ObjectId, Model } from "mongoose";
-import Records from "./BrgyRecords";
+import mongoose, { Document, ObjectId, Model, model } from "mongoose";
 import bcrypt from "bcrypt";
+import dbConnect from "../lib/dbConnect";
+
 const ObjectId = mongoose.Schema.Types.ObjectId;
 const Schema = mongoose.Schema;
+
 
 export interface IUser extends Document {
   username: string;
@@ -13,7 +15,7 @@ export interface IUser extends Document {
 
 export interface IUserModel extends Model<IUser> {
   login(username: string, password: string): Promise<IUser | null>;
-  signup(): Promise<IUser | null>;
+  signup(firstName: string, middleName: string, lastName: string, username: string, password: string): Promise<IUser | null>;
 }
 
 const userSchema = new Schema({
@@ -55,6 +57,37 @@ userSchema.statics.login = async function (
   return user;
 };
 
-// userSchema.statics.signup = async function(firstName: string, middleName: string, lastName: string, username: string, password: string){
+userSchema.statics.signup = async function(firstName: string, middleName: string, lastName: string, username: string, password: string): Promise<IUser| null>{
+  if(!firstName || !lastName || !username || !password){
+    throw new Error("Fill up the everything");
+  }
+  await dbConnect()
+  const exists = await fetch(
+    `http://localhost:3000/api/records/?firstName=${firstName.toUpperCase()}&lastName=${lastName.toUpperCase()}&middleName=${middleName.toUpperCase()}`
+  ) as any;
 
-// }
+  if(exists.ok){
+    const response = await exists.json()
+    console.log(exists);
+    console.log(response);
+    console.log(response?.records._id)
+    console.log(response?.records.firstName)
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password,salt);
+   
+    const user = await this.create({username, password: hash, verified: false, user_id: response?.records._id})
+    return user;
+  }
+  if(!exists){
+    throw new Error("Not a resident");
+  }
+  else{
+    throw new Error("error")
+  }
+
+ 
+}
+
+const User: IUserModel = (mongoose.models.User|| model<IUser, IUserModel>('User', userSchema)) as IUserModel;
+
+export default User;
---
+++ pages/api/user/create_user.ts
@@ -0,0 +1,32 @@
+import { ObjectId } from "mongoose";
+import dbConnect from "../../../lib/dbConnect";
+import User from "../../../models/userAccounts";
+import jwt, { Secret } from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+const createToken = (_id: ObjectId) => {
+  return jwt.sign({ _id }, process.env.SECRET as Secret, { expiresIn: "3d" });
+};
+
+interface Request extends NextApiRequest {
+  _id: ObjectId;
+  privilege: string;
+}
+
+export default async function handler(req: Request, res: NextApiResponse) {
+  const { method } = req;
+  const {firstName, lastName, middleName, username, password } = req.body;
+
+  await dbConnect();
+
+  if (method == "POST") {
+    try {
+      const user = await User.signup(firstName, middleName, lastName, username, password);
+
+      res.status(200).json({ username });
+    } catch (error: any) {
+      res.status(400).json({ error: error?.message, body: req.body });
+    }
+  } else {
+    return res.status(400).json({ success: false });
+  }
+}
