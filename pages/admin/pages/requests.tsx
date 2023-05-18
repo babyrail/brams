31e7671e89186b13c6b472a98654088133402975
dardeejay <dardeejay.calura28@gmail.com>
2023-05-18 14:14:42 +0800

    update

---
+++ pages/admin/pages/requests.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function requests() {
+  return (
+    <div className="ml-48 2xl:ml-56 mt-12   ">
+        <div className="container mx-auto"><h1 className="text-2xl text-primary font-SegoeUI font-bold border-b pb-4">Document Requests</h1></div>
+    </div>
+  )
+}
